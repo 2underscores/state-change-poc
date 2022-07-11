52cf9b3e59220f192151d4b5463a7812ac9887e6
Jeremy Smith <jeremysmith@Jeremys-MacBook-Pro.local>
2022-07-11 20:37:27 +1000

    fix user pool ID

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'tdie4lel0241lto6aatlso716', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'ap-southeast-2_5sT0FiUfc', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'tdie4lel0241lto6aatlso716', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
